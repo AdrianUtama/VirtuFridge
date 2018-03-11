4bf9b8f1149bc1fba260c6b8c656641e6bea7089
wesleyng <wesleyng@tamu.edu>
2018-03-10 23:17:14 -0600

    Allow dialog box to show allow users to enter buttons. Still need to fix double counted items

--- app/src/main/java/virtufridge/virtufridge/ShoppingList.java
+++ app/src/main/java/virtufridge/virtufridge/ShoppingList.java
@@ -2,18 +2,21 @@ package virtufridge.virtufridge;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -24,8 +27,6 @@ public class ShoppingList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         ListView listview;
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -37,13 +38,46 @@ public class ShoppingList extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String key = myRef.child("ShoppingList").push().getKey();
-                myRef.child(key).setValue("Apples");
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(ShoppingList.this);
+                View fillableView = getLayoutInflater().inflate(R.layout.shopping_list_fillable, null);
+                final EditText shoppingItem = (EditText) fillableView.findViewById(R.id.itemName);
+                Button enterItemButton = (Button) fillableView.findViewById(R.id.enterItemButton);
 
+                enterItemButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!shoppingItem.getText().toString().isEmpty()){
+                            //Check to see if item has already been added
+                            DatabaseReference root = FirebaseDatabase.getInstance().getReference("ShoppingList");
+                            DatabaseReference users = root.child("ShoppingList");
+                            users.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot snapshot) {
+                                    if (snapshot.child("ShoppingList").child(shoppingItem.getText().toString()).exists()) {
+                                        Toast.makeText(ShoppingList.this, "Item is already in your list", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        String key = myRef.child("ShoppingList").push().getKey();
+                                        myRef.child(key).setValue(shoppingItem.getText().toString());
+                                        Toast.makeText(ShoppingList.this, "Item added to your list", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
 
+                                }
+                            });
+                        }
+                        else{
+                            Toast.makeText(ShoppingList.this, "Please enter item", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setView(fillableView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
             }
         });
-        //Creates a list view of all items for the customer to read from the database and
+        //Creates a list view of all items for the customer to read from the database and display
         listview=(ListView)findViewById(R.id.listView);
         final ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line,list);
         listview.setAdapter(adapter);
